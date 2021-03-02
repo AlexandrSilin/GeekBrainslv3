bcb8105c3c061fce9b91a77d2338f7ca85c5a5bf
AlexandrSilin <67175@mail.ru>
2021-03-02 17:03:10 +0300

    Fix getWeight()

--- src/lesson1/thirdTask/model/Box.java
+++ src/lesson1/thirdTask/model/Box.java
@@ -5,11 +5,9 @@ import java.util.List;
 
 public class Box <T extends Fruit>{
     private List<T> fruits;
-    private float weight;
 
     public Box(){
         fruits = new ArrayList<>();
-        weight = 0;
     }
 
     public boolean isEmpty(){
@@ -25,15 +23,18 @@ public class Box <T extends Fruit>{
     }
 
     public float getWeight(){
+        float weight = 0;
+        for (Fruit fruit : fruits){
+            weight += fruit.getWeight();
+        }
         return weight;
     }
 
     public void add(T fruit){
         fruits.add(fruit);
-        weight += fruit.getWeight();
     }
     
     public boolean compare(Box box) {
-        return this.weight == box.getWeight();
+        return this.getWeight() == box.getWeight();
     }
 }
