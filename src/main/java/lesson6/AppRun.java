2dfbafedd6b1ab734d8f49d3c94944997b37ea15
AlexandrSilin <67175@mail.ru>
2021-03-25 10:56:58 +0300

    Complete 6 homework

--- src/main/java/lesson6/AppRun.java
+++ src/main/java/lesson6/AppRun.java
@@ -5,6 +5,6 @@ import java.util.Arrays;
 public class AppRun {
     public static void main(String[] args) {
         Array array = new Array();
-        System.out.println(Arrays.toString(array.getBeforeQuadruple(new int[]{2, 14, 2, 3, 4, 1})));
+        System.out.println(Arrays.toString(array.getBeforeQuadruple(new int [] {1, 2, 3, 4, 1})));
     }
 }
--- src/main/java/lesson6/Array.java
+++ src/main/java/lesson6/Array.java
@@ -3,6 +3,17 @@ package lesson6;
 import java.util.Arrays;
 
 public class Array {
+
+    private String getNumbers(int number){
+        int tmp = number;
+        StringBuilder num = new StringBuilder();
+        while (tmp > 0){
+            num.append(tmp % 10);
+            tmp /= 10;
+        }
+        return num.toString();
+    }
+
     public int[] getBeforeQuadruple(int[] arr){
         boolean ex = true;
         int index = 0;
@@ -23,11 +34,21 @@ public class Array {
         boolean one = false;
         boolean four = false;
         for (int i : arr){
-            if (!one && i == 1){
-                one = true;
-            }
-            if (!four && i == 4){
-                four = true;
+            if (i > 9) {
+                String num = getNumbers(i);
+                if (!four && num.contains(Character.toString('4'))){
+                    four = true;
+                }
+                if (!one && num.contains(Character.toString('1'))){
+                    one = true;
+                }
+            } else {
+                if (!one && i == 1) {
+                    one = true;
+                }
+                if (!four && i == 4) {
+                    four = true;
+                }
             }
         }
         return one && four;
